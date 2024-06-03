25ccf4e0075197ba3b70e0b1107c585d9aca96b2
amanu9 <amanuelbeyene47@gmail.com>
2024-06-03 17:14:44 +0300

    im sick

--- client/src/components/shared/Customer.jsx
+++ client/src/components/shared/Customer.jsx
@@ -1,67 +1,68 @@
-import React, { useState, useEffect,useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Outlet } from 'react-router-dom'
 import Header from './Header'
-import CustomerSidebar from './CustomerSidebar';
+import CustomerSidebar from './Sidebar';
 import { useDispatch, useSelector } from 'react-redux';
 import { FaBars } from 'react-icons/fa6';
+import MainContent from './MainContent';
 
 const Customer = () => {
-    const [sidebarVisible, setSidebarVisible] = useState(false);
-    const [drawerVisible, setDrawerVisible] = useState(false);
-    const drawerRef = useRef(null);
+  const [sidebarVisible, setSidebarVisible] = useState(false);
+  const [drawerVisible, setDrawerVisible] = useState(false);
+  const drawerRef = useRef(null);
 
 
-    useEffect(() => {
-        const handleResize = () => {
-          if (window.innerWidth > 768) {
-            setSidebarVisible(true);
-          } else {
-            setSidebarVisible(false);
-          }
-        };
-    
-        window.addEventListener('resize', handleResize);
-        handleResize(); // Call initially to set the sidebar visibility based on the initial screen size
-    
-        return () => {
-          window.removeEventListener('resize', handleResize);
-        };
-      }, []);
-    
-      const toggleDrawer = () => {
-        setDrawerVisible(!drawerVisible);
-      };
-    
-      const handleOutsideClick = (event) => {
-        if (!drawerRef.current.contains(event.target)) {
-          setDrawerVisible(false);
-        }
-      };
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 768) {
+        setSidebarVisible(true);
+      } else {
+        setSidebarVisible(false);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize(); // Call initially to set the sidebar visibility based on the initial screen size
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const toggleDrawer = () => {
+    setDrawerVisible(!drawerVisible);
+  };
+
+  const handleOutsideClick = (event) => {
+    if (!drawerRef.current.contains(event.target)) {
+      setDrawerVisible(false);
+    }
+  };
 
 
-      useEffect(() => {
-        if (drawerVisible) {
-          document.addEventListener('mousedown', handleOutsideClick);
-        } else {
-          document.removeEventListener('mousedown', handleOutsideClick);
-        }
-    
-        return () => {
-          document.removeEventListener('mousedown', handleOutsideClick);
-        };
-      }, [drawerVisible]);
+  useEffect(() => {
+    if (drawerVisible) {
+      document.addEventListener('mousedown', handleOutsideClick);
+    } else {
+      document.removeEventListener('mousedown', handleOutsideClick);
+    }
 
-    return (
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+    };
+  }, [drawerVisible]);
 
-        <div className={`bg-white h-screen w-screen overflow-hidden flex flex-row ${drawerVisible ? 'drawer-visible' : ''}`}>
-            {sidebarVisible && <CustomerSidebar />}
-            <div className="flex flex-col flex-1">
-                <Header />
-                <div className="flex-1 p-4 min-h-0 overflow-auto">
-                    <Outlet />
-                </div>
-            </div>
-            {drawerVisible && (
+  return (
+
+    <div className={`bg-white h-screen w-screen overflow-hidden flex flex-row ${drawerVisible ? 'drawer-visible' : ''}`}>
+      {sidebarVisible && <CustomerSidebar />}
+      <div className="flex flex-col flex-1">
+        <Header />
+        <div className="flex-1 p-4 min-h-0 overflow-auto">
+          <Outlet />
+        </div>
+      </div>
+      {drawerVisible && (
         <div ref={drawerRef} className="fixed inset-0 bg-gray-800 bg-opacity-50 z-50" onClick={toggleDrawer}>
           <div className="h-full flex flex-col justify-between">
             <CustomerSidebar />
@@ -74,9 +75,10 @@ const Customer = () => {
       >
         <FaBars size={24} />
       </button>
-        </div>
+     
+    </div>
 
-    );
+  );
 };
 
 export default Customer
